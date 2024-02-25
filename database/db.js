dfc320cb1358707ed9b1a826c896158c137a4697
Hikmat <94689644+HikmatKhiva@users.noreply.github.com>
2024-02-25 22:22:25 +0500

    Update db.js

--- database/db.js
+++ database/db.js
@@ -1,12 +1,22 @@
 import pkg from "pg";
 import config from "config";
-
+import dotenv from "dotenv";
+// env
+dotenv.config();
 const { Pool } = pkg;
-export const pool = new Pool({
-  connectionString: process.env.POSTGRESQL_URL + "?sslmode=require",
-  // user: config.get("DB_USER"),
-  // host: config.get("DB_HOST"),
-  // password: config.get("DB_PASSWORD"),
-  // port: config.get("DB_PORT"),
-  // database: config.get("DATABASE"),
-});
+let setting_DB;
+if (process.env.NODE_ENV === "development") {
+  setting_DB = {
+    user: config.get("DB_USER"),
+    host: config.get("DB_HOST"),
+    password: config.get("DB_PASSWORD"),
+    port: config.get("DB_PORT"),
+    database: config.get("DATABASE"),
+  };
+}
+if (process.env.NODE_ENV === "production") {
+  setting_DB = {
+    connectionString: process.env.POSTGRESQL_URL + "?sslmode=require",
+  };
+}
+export const pool = new Pool(setting_DB);
